055bb6b9367f8c0e242752066a1ae76f98a1d288
Hayrullah Dogan <hyrllhdgn@gmail.com>
2024-01-08 22:47:20 +0300

    WebElement wait commands

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class LoginTests {
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class LoginTests {
+    WebDriver driver;
+    @BeforeEach
+    void setup(){
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+    @Test
+    void hepsiburadaLoginTesti() throws InterruptedException {
+        driver.get("https://www.hepsiburada.com");
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(200L));
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("myAccount")));
+        driver.findElement(By.id("myAccount")).click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("login")));
+        driver.findElement(By.id("login")).click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("txtUserName")));
+        driver.findElement(By.id("txtUserName")).sendKeys( "seleniumOtomasyonu@gmail.com");
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("btnLogin")));
+        driver.findElement(By.id("btnLogin")).click();
+    }
+    @AfterEach
+    void tearDown(){
+     driver.close();
+    }
 }
